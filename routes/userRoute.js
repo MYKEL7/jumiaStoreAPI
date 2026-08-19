c59493b94da887d9a06e89f1e32691da3b8ee0da
Michael Onyeme <michaelonyeme@capisng.local>
2026-08-19 11:56:54 +0100

    updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router(); //
 
-//import the product controller
 const userController = require('../controllers/userController');
 
 //define the routes
 router.post('/createuser', userController.createUser);
+router.post('/loginuser', userController.loginUser);
 
-router.put('/updateuser/:id', userController.updateUser);
-
-//exprort the router to be used in other files
+//export the router to be used in other files
 module.exports = router;
